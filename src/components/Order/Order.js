db7f810de4b9b72e56595f518bd52dd7aaaee49c
Sergi Juanati <wint3rmute@gmail.com>
2019-12-31 09:36:52 +0100

    Outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import styles from './Order.module.css';
 
-const order = (props) => (
+const order = (props) => {
+
+    // This is equivalent to the transformedIngredients from Burger.js, but a bit simpler
+    // (turn ingredients into an array of objects)
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            });
+    }
+
+    const ingredientOutput = ingredients.map(ing => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px' 
+                }}
+            key={ing.name}> {ing.name} ({ing.amount}) </span>;
+    })
+ 
+    return (
     <div className={styles.Order}>
-        <p>Ingredients: ...</p>
-        <p>Price: <strong>EUR 6.0</strong></p>
+        <p>Ingredients: {ingredientOutput}</p>
+        <p>Price: <strong>EUR {props.price.toFixed(2)}</strong></p>
     </div>
-);
+    );
+};
 
 export default order;
