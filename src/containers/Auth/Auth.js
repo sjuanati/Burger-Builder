c3931c4b096aa0691d2b432aecfd4a05985ccde4
Sergi Juanati <wint3rmute@gmail.com>
2020-01-05 08:46:51 +0100

    Adding actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import styles from './Auth.module.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
     state = {
@@ -83,6 +85,11 @@ class Auth extends Component {
         this.setState({controls: updatedControls});
     }
 
+    submitHandler = (event) => {
+       event.preventDefault();      //prevent reloading of the page
+       this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render () {
         const formElementsArray = [];
         for (let key in this.state.controls) { // key: name, pwd
@@ -105,7 +112,7 @@ class Auth extends Component {
         ));
         return (
             <div className={styles.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">
                         SUBMIT
@@ -116,4 +123,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+
+export default connect(null,mapDispatchToProps)(Auth);
